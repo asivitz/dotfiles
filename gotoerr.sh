194f52d2ed754758c3c9a9d68207b4ec5acd0170
Axis Sivitz <aosivitz@gmail.com>
2017-07-18 21:00:27 -0400

    Goto line/col for errors

--- gotoerr.sh
+++ gotoerr.sh
@@ -2,7 +2,7 @@
 
 # If you have vim in pane 0 and ghcid in pane 1, this will jump to the first error/warning
 
-OP=`tmux capture-pane -p -t 1 | vims -s 'v/:\<enter>nnyVGpVGJ0:s/ //\<enter>0f:d$0P0f:ct/ \<esc>0r+'`
+OP=`tmux capture-pane -p -t 1 | vims -s 'v/:\<enter>nnyVGp:%s/\\\\n//g\<enter>0f:d$0Ps) \<esc>0s+call\\\\ cursor(\<esc>f:r,'`
 
 tmux send-keys Escape
 tmux send-keys ":e $OP"
