759f45d3906ec6cf536a673a420109bf9e9ecebd
Axis Sivitz <aosivitz@gmail.com>
2020-12-28 23:53:09 -0500

    Small tweak

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2_profile"
+# defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2_profile"
 # Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+# defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 cd
 
